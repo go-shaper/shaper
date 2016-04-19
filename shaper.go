e911d9ff2c7e648a80dec3ba004bfbfbfb72f6e1
Tong Sun <suntong@cpan.org>
2016-04-18 22:56:14 -0400

    - [!] correct golint warning, receiver name should be a reflection of its identity; dont use generic names such as "me", "this", or "self"

--- shaper.go
+++ shaper.go
@@ -47,51 +47,51 @@ func NewFilter() *Shaper {
 // Copy returns a copy of the original object, instead of editing in-place,
 // so make sure you've already got a reference to the original
 // This should NEVER be hung off of a NewFilter string, or the original NewFilter will be lost
-func (me *Shaper) Copy() *Shaper {
+func (shaper *Shaper) Copy() *Shaper {
 	return &Shaper{
-		ShaperStack: me.ShaperStack,
+		ShaperStack: shaper.ShaperStack,
 	}
 }
 
 // Process will actually process a string using the built-up filter chain
-func (me *Shaper) Process(s string) string {
-	return me.ShaperStack(s)
+func (shaper *Shaper) Process(s string) string {
+	return shaper.ShaperStack(s)
 }
 
 // AddFilter is used to apply arbitrary filters
-func (me *Shaper) AddFilter(f func(string) string) *Shaper {
-	me.ShaperStack = func(a func(string) string, b func(string) string) func(string) string {
+func (shaper *Shaper) AddFilter(f func(string) string) *Shaper {
+	shaper.ShaperStack = func(a func(string) string, b func(string) string) func(string) string {
 		return func(s string) string {
 			return a(b(s))
 		}
-	}(f, me.ShaperStack)
-	return me
+	}(f, shaper.ShaperStack)
+	return shaper
 }
 
 // ApplyToLower will apply/add the strings.ToLower filter to the Shaper
-func (me *Shaper) ApplyToLower() *Shaper {
-	me.AddFilter(strings.ToLower)
-	return me
+func (shaper *Shaper) ApplyToLower() *Shaper {
+	shaper.AddFilter(strings.ToLower)
+	return shaper
 }
 
 // ApplyToUpper will apply/add the strings.ToUpper filter to the Shaper
-func (me *Shaper) ApplyToUpper() *Shaper {
-	me.AddFilter(strings.ToUpper)
-	return me
+func (shaper *Shaper) ApplyToUpper() *Shaper {
+	shaper.AddFilter(strings.ToUpper)
+	return shaper
 }
 
 // ApplyReplace will apply/add the strings.Replace filter to the Shaper
-func (me *Shaper) ApplyReplace(old, new string, times int) *Shaper {
-	me.AddFilter(func(s string) string {
+func (shaper *Shaper) ApplyReplace(old, new string, times int) *Shaper {
+	shaper.AddFilter(func(s string) string {
 		return strings.Replace(s, old, new, times)
 	})
-	return me
+	return shaper
 }
 
 // ApplyRegexpReplaceAll will apply/add the regexp.ReplaceAllString filter to the Shaper
-func (me *Shaper) ApplyRegexpReplaceAll(rexp, repl string) *Shaper {
-	me.AddFilter(func(s string) string {
+func (shaper *Shaper) ApplyRegexpReplaceAll(rexp, repl string) *Shaper {
+	shaper.AddFilter(func(s string) string {
 		return regexp.MustCompile(rexp).ReplaceAllString(s, repl)
 	})
-	return me
+	return shaper
 }
