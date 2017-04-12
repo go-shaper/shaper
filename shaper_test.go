6666e248b5d4517b01fa458346ccce3aac4ce186
Tong Sun <suntong@cpan.org>
2017-04-12 12:55:35 -0400

    - [+] add chaining exiting filters

--- shaper_test.go
+++ shaper_test.go
@@ -103,8 +103,11 @@ func ExampleShaper() {
 	// == Extending shaper.Shaper to add your own filters
 	var hu *Shaper
 	hu = NewFilter()
+	hu.ApplyToUpper()
+	fmt.Printf("%s\n", hu.Process("2 &gt;= 1"))
 	hu.ApplyHTMLUnescape()
 	fmt.Printf("%s\n", hu.Process("2 &gt;= 1"))
 	// Output:
+	// 2 &GT;= 1
 	// 2 >= 1
 }
